53f8f537c11152781624d9b8d7b8011275f66c54
Gebraiel <67439330+Gebraiel@users.noreply.github.com>
2024-10-17 21:51:49 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.get('/users',(req,res)=>{
 
     res.status(200).json(users);
   } catch (error) {
-    res.status(500).json({ message: "Server Error" });
+    res.status(500).json({ message: "Error : Server Error!!" });
   }
 });
 const port = process.env.PORT || 8000;
