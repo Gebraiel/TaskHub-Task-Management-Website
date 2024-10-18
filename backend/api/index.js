fe11a54d57ec37c5a7827b0652bcb0155f2a610e
Gebraiel <67439330+Gebraiel@users.noreply.github.com>
2024-10-18 21:59:22 +0300

    Create index.js

---
+++ backend/api/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const taskRouter = require("./routers/taskRouter");
+const userRouter = require("./routers/userRouter");
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(morgan("dev"));
+dotenv.config();
+
+const dbConnect = require("./config/dbConnect");
+
+app.get(`/`, (req, res) => {
+  console.log("Hello API!!!!");
+
+  res.send(`Hello API`);
+});
